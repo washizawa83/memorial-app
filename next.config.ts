050ec53648038d80ce07964d65fb9cc73ba614b2
washizawa haruya (JSL) <129074090+washizawa83@users.noreply.github.com>
2024-11-25 21:15:47 +0900

    環境設定 (#1)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    /* config options here */
+    output: 'export',
+    distDir: 'dist',
+}
 
-export default nextConfig;
+export default nextConfig
